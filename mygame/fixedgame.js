7b4081cbd133e8e06dedc394f4a89c0f971c8b78
ELECTROMONKEY24 <yssmc24@gmail.com>
2024-06-03 20:41:19 +0530

    code fix

--- mygame/fixedgame.js
+++ mygame/fixedgame.js
@@ -3,129 +3,78 @@ import jsonData from './data.json';
 import './game.css'; // Import CSS file for styling
 import correctaudio from '../Audio/correct.mp3';
 import wrongaudio from '../Audio/hooray.mp3';
+import Confetti from 'react-confetti';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const DragAndDropTable = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [touches, setTouches] = useState([]);
   const [tries, setTries] = useState(0);
-  const [arrayOfTries, setArrayOfTries] = useState([]);
+  const [correctTries, setCorrectTries] = useState(0);
+  const [showConfetti, setShowConfetti] = useState(false);
 
   useEffect(() => {
-    setTries(0);
-    if (currentPage === 4) {
-      logData();
-    }
+    setCorrectTries(0);
+    resetButtonsVisibility();
   }, [currentPage]);
 
-  const dragStart = (event) => {
-    if (event.type === "touchstart") {
-      setTouches([event.changedTouches[0]]);
-    } else {
-      event.dataTransfer.setData("text", event.target.id);
+  const onDragEnd = result => {
+    const { destination, draggableId } = result;
+
+    if (!destination) {
+      return;
     }
-  };
 
-  const drag = (event) => {
-    event.dataTransfer.setData("text", event.target.id);
-  };
-
-  const allowDrop = (event) => {
-    event.preventDefault();
-  };
-
-  const drop = (eventOrData, dropZone) => {
-    if (eventOrData instanceof Event) {
-      eventOrData.preventDefault();
-    }
-    const data = eventOrData.dataTransfer? eventOrData.dataTransfer.getData("text") : eventOrData.id;
-    const draggedElement = document.getElementById(data);
-    const target = dropZone;
+    const draggedElement = document.getElementById(draggableId);
+    const target = document.querySelector(`[data-type="${destination.droppableId}"]`);
     const targetType = target.getAttribute('data-type');
     const draggedType = draggedElement.getAttribute('data-type');
 
-    if (target.tagName === 'TD') {
-      if (targetType === draggedType) {
-        setTries(prevTries => prevTries + 1);
-        const clonedElement = draggedElement.cloneNode(true);
-        target.appendChild(clonedElement);
-        const audio = new Audio(correctaudio);
-        audio.play();
-        clonedElement.classList.add('correct-drop');
-        clonedElement.classList.add('hover-animation');
-        setTimeout(() => {
-          if (clonedElement) {
-            clonedElement.classList.remove('correct-drop');
-            clonedElement.classList.remove('hover-animation');
-          }
-        }, 500);
-        draggedElement.style.display = 'none';
-      } else {
-        setTries(prevTries => prevTries + 1);
-        const audio = new Audio(wrongaudio);
-        audio.play();
-        draggedElement.classList.add('wrong-drop');
-        setTimeout(() => {
-          if (draggedElement) {
-            draggedElement.classList.remove('wrong-drop');
-          }
-        }, 500);
-      }
-    }
-  };
-
-  const touchMove = (event) => {
-    event.preventDefault();
-    if (event.type === "touchmove") {
-      const touch = event.changedTouches[0];
-      const draggedElement = document.getElementById(touches[0].target.id);
-      const rect = draggedElement.getBoundingClientRect();
-      const x = touch.clientX - rect.left;
-      const y = touch.clientY - rect.top;
-      draggedElement.style.transform = `translate(${x}px, ${y}px)`;
-    }
-  };
-
-  const touchEnd = (event) => {
-    event.preventDefault();
-    if (event.type === "touchend") {
-      const touch = event.changedTouches[0];
-      const draggedElement = document.getElementById(touches[0].target.id);
-      const rect = draggedElement.getBoundingClientRect();
-      const data = {
-        id: draggedElement.id,
-        x: touch.clientX - rect.left,
-        y: touch.clientY - rect.top
-      };
-      draggedElement.style.removeProperty('transform');
-      const dropZone = document.elementFromPoint(touch.clientX, touch.clientY);
-      drop(data, dropZone);
-      setTouches([]);
+    if (targetType === draggedType) {
+      setTries(prevTries => prevTries + 1);
+      const clonedElement = draggedElement.cloneNode(true);
+      clonedElement.draggable = false; // Prevent further dragging
+      clonedElement.style.userSelect = 'none'; // Disable text selection
+      target.appendChild(clonedElement);
+      const audio = new Audio(correctaudio);
+      audio.play();
+      setCorrectTries(prevCorrectTries => prevCorrectTries + 1);
+      clonedElement.classList.add('correct-drop');
+      clonedElement.classList.add('hover-animation');
+      setTimeout(() => {
+        if (clonedElement) {
+          clonedElement.classList.remove('hover-animation');
+        }
+      }, 500);
+      draggedElement.style.display = 'none';
+    } else {
+      setTries(prevTries => prevTries + 1);
+      const audio = new Audio(wrongaudio);
+      audio.play();
+      draggedElement.classList.add('wrong-drop');
+      setTimeout(() => {
+        if (draggedElement) {
+          draggedElement.classList.remove('wrong-drop');
+        }
+      }, 500);
     }
   };
 
   const nextPage = () => {
-    setArrayOfTries(prevArray => [...prevArray, tries]);
     setCurrentPage(prevPage => prevPage + 1);
   };
 
   const logData = () => {
-    setArrayOfTries(prevArray => [...prevArray, tries]);
-    console.log('The number of tries for each page are:', arrayOfTries);
+    console.log("The number of tries:", tries);
   };
 
   const createTable = () => {
     const currentItem = jsonData[currentPage.toString()];
-
-    const headers = Object.keys(currentItem).filter(key => key!== 'Distracter');
+    const headers = Object.keys(currentItem).filter(key => key !== 'Distracter');
 
     const getTableCell = header => {
       return (
         <td
           key={header}
-          onDrop={(event) => drop(event, event.target)}
-          onDragOver={allowDrop}
-          onTouchMove={touchMove}
-          onTouchEnd={touchEnd}
           data-type={header}
           className="table-cell"
         ></td>
@@ -141,11 +90,11 @@ const DragAndDropTable = () => {
     const tableCells = headers.map(header => getTableCell(header));
 
     return (
-      <table className="custom-table">
+      <table className="custom-table mt-3">
         <thead>
           <tr>{tableHeaders}</tr>
         </thead>
-<tbody>
+        <tbody>
           <tr>{tableCells}</tr>
         </tbody>
       </table>
@@ -159,7 +108,7 @@ const DragAndDropTable = () => {
     // Collect all cards including main and distracter cards
     const allCards = [];
     for (const key in currentItem) {
-      if (key!== 'Distracter') {
+      if (key !== 'Distracter') {
         const values = currentItem[key];
         values.forEach((value, index) => {
           const card = {
@@ -188,19 +137,20 @@ const DragAndDropTable = () => {
     // Create JSX elements for shuffled cards
     shuffledCards.forEach(card => {
       const jsxCard = (
-        <div
-          key={card.id}
-          id={card.id}
-          className="draggable"
-          draggable
-          onTouchStart={(event) => dragStart(event)}
-          onTouchMove={(event) => touchMove(event)}
-          onTouchEnd={(event) => touchEnd(event)}
-          onDragStart={(event) => drag(event)}
-          data-type={card.type}
-        >
-          {card.value}
-        </div>
+        <Draggable key={card.id} draggableId={card.id} index={cards.length}>
+          {(provided) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              id={card.id}
+              className="draggable"
+              data-type={card.type}
+            >
+              {card.value}
+            </div>
+          )}
+        </Draggable>
       );
       cards.push(jsxCard);
     });
@@ -217,40 +167,87 @@ const DragAndDropTable = () => {
     return array;
   };
 
+  const handleSubmit = () => {
+    const currentItem = jsonData[currentPage.toString()];
+    const numberOfDraggableCards = Object.keys(currentItem).reduce((sum, key) => {
+      if (key !== 'Distracter') {
+        sum += currentItem[key].length;
+      }
+      return sum;
+    }, 0);
+    const numberOfCorrectDropsNeeded = numberOfDraggableCards;
+    if (correctTries === numberOfCorrectDropsNeeded) {
+      const nextButton = document.getElementById('nextbutton');
+      nextButton.style.display = 'block';
+      const submitButton = document.getElementById('submitbutton');
+      submitButton.style.display = 'none';
+      setShowConfetti(true);
+      setTimeout(() => setShowConfetti(false), 5000); // Confetti for 5 seconds
+    }
+  };
+
+  const resetButtonsVisibility = () => {
+    const nextButton = document.getElementById('nextbutton');
+    const submitButton = document.getElementById('submitbutton');
+    if (nextButton) nextButton.style.display = 'none';
+    if (submitButton) submitButton.style.display = 'block';
+  };
+
   return (
     <div className="body container-fluid maindiv">
-      <h1 id="title" className="text-center">Drag and Drop Table</h1>
-      <p id="tries-counter" className="text-center">Tries: {tries}</p>
       {createTable()}
       <div className="cardposition">
-        <div className="cards-container justify-content-center mb-2">{createCards()}</div>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="cards" direction="horizontal">
+            {(provided) => (
+              <div
+                className="cards-container justify-content-center align-items-center mb-2"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {createCards()}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </div>
-      <div className="next">
-        <button
-          style={{ display: currentPage < 4? 'block' : 'none' }}
-          onClick={nextPage}
-          id="nextbutton"
-          className='btn btn-custom btn-block'
-        >
-          Next
-        </button>
+      <div className="buttons">
+        {currentPage < Object.keys(jsonData).length ? (
+          <div className='nextbutton d-flex justify-content-center align-items-center'>
+            <button
+              style={{ display: 'none' }}
+              onClick={nextPage}
+              id="nextbutton"
+              className='btn btn-custom btn-block'
+            >
+              Next
+            </button>
+          </div>
+        ) : (
+          <div className='logdata d-flex justify-content-center align-items-center'> 
+            <button
+              id="logdata"
+              className='btn btn-custom btn-block'
+              style={{ display: currentPage > Object.keys(jsonData).length - 1 ? 'block' : 'none' }}
+              onClick={logData}
+            >
+              Logdata
+            </button>
+          </div>
+        )}
+        <div className="submitbutton d-flex justify-content-center align-items-center">
+          <button
+            style={{ display: 'block' }}
+            onClick={handleSubmit}
+            id="submitbutton"
+            className='btn btn-custom btn-primary mt-2'
+          >
+            Submit
+          </button>
+        </div>
       </div>
-      <button
-        onClick={() => window.location.reload()}
-        id="Restart"
-        className='btn btn-custom btn-block'
-        style={{ display: currentPage > 3? 'block' : 'none' }}
-      >
-        Restart
-      </button>
-      <button
-        id="logdata"
-        className='btn btn-custom btn-block'
-        style={{ display: currentPage > 3? 'block' : 'none' }}
-        onClick={logData}
-      >
-        Logdata
-      </button>
+      {showConfetti && <Confetti width={window.innerWidth} height={window.innerHeight}/>}
     </div>
   );
 };
