1eb57042ee7315c70f76df14cff6bd09f896a2f8
ELECTROMONKEY24 <yssmc24@gmail.com>
2024-06-07 14:30:57 +0530

    routes modification

--- src/App.js
+++ src/App.js
@@ -3,17 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import DragAndDropTable from "./Components/Game";
 import Home from "./Components/Home";
-import Dndtable from "./Components/Gamewithstyling";
 import './Components/game.css';
 export default function App(){
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Dndkit />} /> */}
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<DragAndDropTable />} />
-        <Route path="/rctgame" element={<Dndtable />} />
-        {/* <Route path="/rctgame" element={<Dndtable />} /> */}
       </Routes>
     </Router>
   );
